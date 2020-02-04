a910f790520d5fce4d709ef2a60068e95822a9c0
aakosa3 <alexandra.akosa@gmail.com>
2020-02-04 16:18:57 +0100

    artist travel

---
+++ mobilitymapscript.R
@@ -0,0 +1,20 @@
+library(sf)
+library(mapdeck)
+
+key <- "pk.eyJ1IjoibWllZGVtYXRlcyIsImEiOiJjazV4dmVkbXcyMzQ3M2ttbG5kbzU3bWt4In0.G3RNl0RBO_tWTY1wYrv7tQ"
+set_token(key)
+
+artist_travel <- read.csv('input/artist_travel.csv',header = TRUE,sep=";")
+
+mapdeck( token = key, style = 'mapbox://styles/mapbox/dark-v9', pitch = 45 ) %>%
+  add_arc(
+    data = artist_travel
+    , layer_id = "arc_layer"
+    , origin = c("start_lon", "start_lat")
+    , destination = c("end_lon", "end_lat")
+    , stroke_from = "from"
+    , stroke_to = "to"
+    , stroke_width = "stroke"
+    , auto_highlight = TRUE
+    , tooltip = 'artist'
+  )
