68cd87b43de05229701fa31aa2346b988b620336
florindevessies <florindevessies@users.noreply.github.com>
2020-02-25 15:41:27 +0100

    updated stacked radial barchart

--- Stacked_radial_barchart/Script_radial_barchart2.js
+++ Stacked_radial_barchart/Script_radial_barchart2.js
@@ -8,7 +8,7 @@ Edited for this context by DGTL x AMS LL
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height"),
-    innerRadius = 180,
+    innerRadius = 100,
     outerRadius = Math.min(width, height) / 2,
     g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
@@ -38,16 +38,48 @@ d3.csv("test_data.csv", function(d, i, columns) {
   x.domain(data.map(function(d) { return d.Impact; }));
   // create the maximum value of the outer range
   y.domain([0, d3.max(data, function(d) { return d.total + 300; })]);
-  z.domain(data.columns.slice(1));
+  z.domain(data.columns.slice(10));
 
+// Make sure the hover works!
+  var defs = svg.append("defs");
+var filter = defs.append("filter")
+                .attr("id", "drop-shadow")
+                .attr("height","130%");
+
+// create shadow/blur around the pie parts
+filter.append("feGaussianBlur")
+        .attr("in","SourceAlpha")
+        .attr("stdDeviation", 8)
+        .attr("result", "blur");
+
+filter.append("feOffset")
+    .attr("in", "blur")
+    .attr("dx", 3)
+    .attr("dy", 3)
+    .attr("result", "offsetBlur");
+    var feMerge = filter.append("feMerge");
+
+feMerge.append("feMergeNode")
+    .attr("in", "offsetBlur")
+feMerge.append("feMergeNode")
+    .attr("in", "SourceGraphic");
+
+/*  var idu = svg.append("idu");
+  var unique = idu.append("unique")
+  .attr("id", 'drop-shadow');*/
+console.log(g.data(d3.stack().keys(data.columns.slice(1))(data)));
   // drawing the bars
   g.append("g")
     .selectAll("g")
     .data(d3.stack().keys(data.columns.slice(1))(data))
     .enter().append("g")
-      .attr("fill", function(d) { return z(d.key); })
+      .attr("fill", function(d) { 
+        return z(d.key); })
     .selectAll("path")
-    .data(function(d) { return d; })
+    // I want to append the key to the parts of the g, so I can select these
+    .data(function(d) { 
+      console.log(d.key)
+      return d; })
     .enter().append("path")
       .attr("d", d3.arc()
           .innerRadius(function(d) { return y(d[0]); })
@@ -58,27 +90,25 @@ d3.csv("test_data.csv", function(d, i, columns) {
           .padRadius(innerRadius))
     .on("mousemove", function(d) {
           d3.select(this)
+          // add stroke around the parts (white in this case)
               .attr("stroke","#fff")
               .attr("stroke-width","2px")
-              .style("filter", "url(#drop-shadow)");
-          d3.select(this)
-            .transition()
-            .duration(500)
-           // .ease('elastic')
-            .attr('transform',function(d){
-              var dist = 1;
-              d.midAngle = ((d.endAngle - d.startAngle)/2) + d.startAngle;
-              var x = Math.sin(d.midAngle) * dist;
-              var y = Math.cos(d.midAngle) * dist;
-              return 'translate(' + x + ',' + y + ')';
-            });  
+              .style("filter", "url(#drop-shadow)")
+              .append("g:title")
+              .text(function(d) { 
+                res = ""; 
+                Object.keys(d.data).forEach(function(key, idx){
+                  res += key + ": " +  d.data[key] + "\n"; 
+                }); 
+
+              return res }); 
            var mousePos = d3.mouse(divNode);
             d3.select("#mainTooltip")
               .style("left", mousePos[0] - 40 + "px")
               .style("top", mousePos[1] - 70 + "px")
               .select("#value")
               .attr("text-anchor", "middle")
-              .html(d.data.str_lab + "<br />" + d.data.num);
+              //.html(d.data.str_lab + "<br />" + d.data.num);
 
             d3.select("#mainTooltip").classed("hidden", false);
         })
@@ -93,7 +123,13 @@ d3.csv("test_data.csv", function(d, i, columns) {
             .attr('transform','translate(0,0)');
 
           d3.select("#mainTooltip").classed("hidden", true);
-      });
+      })
+      .on("click", function() 
+        { window.open("https://dgtl.nl/select-your-edition/"); 
+        //$(location).attr('href', url);
+        //window.location = url;    
+    });
+
 
       // creating ALL labels
   var label = g.append("g")
@@ -120,16 +156,19 @@ d3.csv("test_data.csv", function(d, i, columns) {
   var yAxis = g.append("g")
       .attr("text-anchor", "middle");
 
+      console.log("BLAAAAA",y, y.ticks(2));
   var yTick = yAxis
     .selectAll("g")
     // create number of circles!
-    .data(y.ticks(5).slice(1))
+    .data([100])
     .enter().append("g");
 
   // draw the circles
   yTick.append("circle")
       .attr("fill", "none")
-      .attr("stroke", "#000")
+      .attr("stroke", "red")
+      .attr("stroke-width", "10px")
+      .attr("on:hover", "stroke:green")
       .attr("r", y);
 
   yTick.append("text")
@@ -148,7 +187,7 @@ d3.csv("test_data.csv", function(d, i, columns) {
       .text(y.tickFormat(5, "s"));
 
   yAxis.append("text")
-      .attr("y", function(d) { return -y(y.ticks(5).pop()); })
+      .attr("y", function(d) { return -y(y.ticks(10).pop()); })
       .attr("dy", "-1em")
       .text("Impact");
 
@@ -169,4 +208,20 @@ d3.csv("test_data.csv", function(d, i, columns) {
       .attr("y", 9)
       .attr("dy", "0.35em")
       .text(function(d) { return d; });
+});
+
+console.log(d3.select('path'));   
+
+$(document).ready(() => {
+
+    $('path').click(function(evt){
+      var elm = $(evt.target); 
+      elm.attr('stroke', 'red'); 
+  });
+}); 
+
+$(document).ready(function(){
+  $("button").click(function(){
+    alert("Image width: " + $("img").attr("width"));
+  });
 });
