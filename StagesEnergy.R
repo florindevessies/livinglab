3186f99be4644287c09344af330d40905398f54b
florindevessies <florindevessies@users.noreply.github.com>
2020-03-19 15:54:47 +0100

    energy stages again

--- StagesEnergy.R
+++ StagesEnergy.R
@@ -15,11 +15,11 @@ flows2$stroke <- sample(1:3, size = nrow(flows2), replace = T)
 
 
 
-dgtl$height <- 7.0
-dgtl[1,"height"] <- 12.0
-dgtl[2,"height"] <- 4.0
-dgtl[3,"height"] <- 30.0
-dgtl[4,"height"] <- 8.0
+dgtl$height <- 10
+dgtl[1,"height"] <- 10.0
+dgtl[2,"height"] <- 10
+dgtl[3,"height"] <- 10
+dgtl[4,"height"] <- 10
 
 dgtl$color <- "#4F364D"
 dgtl[1,"color"]<-"#E9EFBF"
@@ -53,46 +53,46 @@ dgtl[26,"color"]<- "#186134"
 dgtl[27,"color"]<- "#186134"
 
 flows2$color_from <- "color"
-flows2[1,"color_from"] <- "#186134"
-flows2[2,"color_from"] <- "#186134"
-flows2[3,"color_from"] <- "#186134"
-flows2[4,"color_from"] <- "#737574"
-flows2[5,"color_from"] <- "#186134"
-flows2[6,"color_from"] <- "#186134"
-flows2[7,"color_from"] <- "#186134"
-flows2[8,"color_from"] <- "#186134"
-flows2[9,"color_from"] <- "#186134"
-flows2[10,"color_from"] <- "#737574"
-flows2[11,"color_from"] <- "#737574"
-flows2[12,"color_from"] <- "#186134"
-flows2[13,"color_from"] <- "#186134"
-flows2[14,"color_from"] <- "#186134"
-flows2[15,"color_from"] <- "#737574"
-flows2[16,"color_from"] <- "#186134"
-flows2[17,"color_from"] <- "#186134"
-flows2[18,"color_from"] <- "#186134"
-flows2[19,"color_from"] <- "#186134"
+flows2[1,"color_from"] <- "#14f593"
+flows2[2,"color_from"] <- "#14f593"
+flows2[3,"color_from"] <- "#14f593"
+flows2[4,"color_from"] <- "#fa0505"
+flows2[5,"color_from"] <- "#14f593"
+flows2[6,"color_from"] <- "#14f593"
+flows2[7,"color_from"] <- "#14f593"
+flows2[8,"color_from"] <- "#14f593"
+flows2[9,"color_from"] <- "#14f593"
+flows2[10,"color_from"] <- "#fa0505"
+flows2[11,"color_from"] <- "#fa0505"
+flows2[12,"color_from"] <- "#14f593"
+flows2[13,"color_from"] <- "#14f593"
+flows2[14,"color_from"] <- "#14f593"
+flows2[15,"color_from"] <- "#fa0505"
+flows2[16,"color_from"] <- "#14f593"
+flows2[17,"color_from"] <- "#14f593"
+flows2[18,"color_from"] <- "#14f593"
+flows2[19,"color_from"] <- "#14f593"
 
 flows2$color_to <- "color"
-flows2[1,"color_to"] <- "#0BCC50"
-flows2[2,"color_to"] <- "#0BCC50"
-flows2[3,"color_to"] <- "#0BCC50"
-flows2[4,"color_to"] <- "#737574"
-flows2[5,"color_to"] <- "#0BCC50"
-flows2[6,"color_to"] <- "#0BCC50"
-flows2[7,"color_to"] <- "#0BCC50"
-flows2[8,"color_to"] <- "#0BCC50"
-flows2[9,"color_to"] <- "##0BCC50"
-flows2[10,"color_to"] <- "#737574"
-flows2[11,"color_to"] <- "#737574"
-flows2[12,"color_to"] <- "#0BCC50"
-flows2[13,"color_to"] <- "#0BCC50"
-flows2[14,"color_to"] <- "#0BCC50"
-flows2[15,"color_to"] <- "#737574"
-flows2[16,"color_to"] <- "#0BCC50"
-flows2[17,"color_to"] <- "#0BCC50"
-flows2[18,"color_to"] <- "#0BCC50"
-flows2[19,"color_to"] <- "#0BCC50"
+flows2[1,"color_to"] <- "#14f593"
+flows2[2,"color_to"] <- "#14f593"
+flows2[3,"color_to"] <- "#14f593"
+flows2[4,"color_to"] <- "#fa0505"
+flows2[5,"color_to"] <- "#14f593"
+flows2[6,"color_to"] <- "#14f593"
+flows2[7,"color_to"] <- "#14f593"
+flows2[8,"color_to"] <- "#14f593"
+flows2[9,"color_to"] <- "#14f593"
+flows2[10,"color_to"] <- "#fa0505"
+flows2[11,"color_to"] <- "#fa0505"
+flows2[12,"color_to"] <- "#14f593"
+flows2[13,"color_to"] <- "#14f593"
+flows2[14,"color_to"] <- "#14f593"
+flows2[15,"color_to"] <- "#fa0505"
+flows2[16,"color_to"] <- "#14f593"
+flows2[17,"color_to"] <- "#14f593"
+flows2[18,"color_to"] <- "#14f593"
+flows2[19,"color_to"] <- "#14f593"
 
 
 dgtl$fill_opacity <- 225
@@ -106,15 +106,15 @@ dgtl[6, "fill_colour"] <- "#3498DB"
 dgtl[7, "fill_colour"] <- "#3498DB"
 
 
-dgtl$hover <- "Generatior <br> kWh <br> CO2 <br> NOx"
-dgtl[1,"hover"]<-"Modular <br> kWh <br> CO2 <br> Nox "
-dgtl[2,"hover"]<- "Generator <br> kWh <br> CO2 <br> NOx"
-dgtl[3,"hover"]<- "Skyline <br> kWh <br> Co2 <br> NOx"
-dgtl[4,"hover"]<- "Kornuit <br> kWh <br> CO2 <br> NOx"
-dgtl[5,"hover"]<- "Art <br> kWh <br> CO2 <br> NOx"
-dgtl[6,"hover"]<- "AMP <br> kWh <br> CO2 <br> NOx"
-dgtl[7,"hover"]<- "Live <br> kWh <br> CO2 <br> NOx"
-dgtl[8,"hover"]<- "foodcourt <br> kWh <br> CO2 <br> NOx"
+dgtl$hover <- "Generatior <br> kWh 326 <br> CO2 0"
+dgtl[1,"hover"]<-"Modular <br> kWh 4742 <br> CO2 0"
+dgtl[2,"hover"]<- "Generator <br> kWh 326 <br> CO2 0"
+dgtl[3,"hover"]<- "Skyline <br> kWh 844 <br> CO2 0 "
+dgtl[4,"hover"]<- "Kornuit <br> kWh <br> CO2 0 "
+dgtl[5,"hover"]<- "Art <br> kWh 970 <br> CO2 0  "
+dgtl[6,"hover"]<- "AMP <br> kWh 2298 <br> CO2 0 "
+dgtl[7,"hover"]<- "Live <br> kWh 960 <br> CO2 0"
+dgtl[8,"hover"]<- "foodcourt <br> kWh 2964 <br> CO2 1648 kg "
 
 dgtl[9,"hover"]<-"L1"
 dgtl[10,"hover"]<- "L2"
@@ -125,7 +125,7 @@ dgtl[14,"hover"]<- "L6"
 dgtl[15,"hover"]<- "L7"
 dgtl[16,"hover"]<- "L8"
 dgtl[17,"hover"]<- "L9"
-dgtl[18,"hover"]<- "L10"
+dgtl[18,"hover"]<- "L10 <br> CO2 422.5 kg"
 dgtl[19,"hover"]<- "L11"
 dgtl[20,"hover"]<-"L12"
 dgtl[21,"hover"]<- "L13"
@@ -143,7 +143,7 @@ key <- "pk.eyJ1IjoibWllZGVtYXRlcyIsImEiOiJjazV4dmVkbXcyMzQ3M2ttbG5kbzU3bWt4In0.G
 
 # Initialize mapdeck
 # https://symbolixau.github.io/mapdeck/articles/mapdeck.html#the-basics
-mapdeck(token = key, style = mapdeck_style("dark"), pitch = 30) %>%
+mapdeck(token = key, style = mapdeck_style("dark"), pitch = 50, zoom = 100) %>%
   add_polygon(
     data = dgtl
     , layer = "polygon_layer"
@@ -161,5 +161,5 @@ add_arc(
   , origin = c("start_long", "start_lat")
   , destination = c("end_long", "end_lat")
   , stroke_from = "color_from"
-  , stroke_to = "color_to"
+  , stroke_to = "color_to" 
     , stroke_width = "stroke")
