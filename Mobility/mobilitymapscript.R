b338bbb6c1707957fc736aedce2f8bda6b206981
florindevessies <florindevessies@users.noreply.github.com>
2020-04-21 14:12:01 +0200

    organising github

--- Mobility/mobilitymapscript.R
+++ Mobility/mobilitymapscript.R
@@ -1,9 +1,14 @@
+#   Script for the mobility map datavisualisation for DGTL festival
+# Edited for this context by DGTL x AMS LL
+
+                                        
 library(sf)
 library(mapdeck)
 
 key <- "pk.eyJ1IjoibWllZGVtYXRlcyIsImEiOiJjazV4dmVkbXcyMzQ3M2ttbG5kbzU3bWt4In0.G3RNl0RBO_tWTY1wYrv7tQ"
 set_token(key)
 
+# load the datafile
 artist_travel <- read.csv('input/artist_travel.csv',header = TRUE,sep=";")
 
 mapdeck( token = key, style = 'mapbox://styles/mapbox/dark-v9', pitch = 0, padding = 0  ) %>%
@@ -19,3 +24,5 @@ mapdeck( token = key, style = 'mapbox://styles/mapbox/dark-v9', pitch = 0, paddi
     , auto_highlight = TRUE
     , tooltip = 'mode'
   )
+
+#export the the map as html page and save it in the output folder to make it available online
